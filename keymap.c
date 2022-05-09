7f1217c1494c0047f3e354e0b25fa6beee86ff0f
Patrick Tsai <patrick@simplypatrick.com>
2022-05-08 20:41:51 -0700

    tap-dance: slashes for / \

--- keymap.c
+++ keymap.c
@@ -203,6 +203,7 @@ enum tapdance_keycodes {
     TD_R_BRACKETS,
     TD_QUOTES,
     TD_SEMICOLON,
+    TD_SLASHES,
 };
 
 // Tap Dance Definitions
@@ -218,12 +219,16 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
     // once: semicolon, twice: end and semicolon
     [TD_SEMICOLON] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_semicolon_finished, td_semicolon_reset),
+
+    // once: slash, twice: back slash
+    [TD_SLASHES] = ACTION_TAP_DANCE_DOUBLE(KC_SLASH, KC_BACKSLASH),
 };
 
 #define TD_LBRK TD(TD_L_BRACKETS)
 #define TD_RBRK TD(TD_R_BRACKETS)
 #define TD_QUOT TD(TD_QUOTES)
 #define TD_SCLN TD(TD_SEMICOLON)
+#define TD_SLSH TD(TD_SLASHES)
 
 // Layers
 
@@ -259,7 +264,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD_QUOT,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  TD_SLSH, KC_RSFT,
     BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
