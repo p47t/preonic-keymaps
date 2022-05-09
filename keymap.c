e43be9a1c5f8551df7b2bdf5956e2899c073d2ce
Patrick Tsai <patrick@simplypatrick.com>
2022-05-08 22:02:24 -0700

    base: low-left key to toggle microphone mute

--- keymap.c
+++ keymap.c
@@ -284,29 +284,30 @@ enum preonic_keycodes {
     LOWER = SAFE_RANGE,
     RAISE,
     BACKLIT,
+    MICMUTE, // mute microphone
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | '"`  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   `  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Enter|
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /\  | Shift|
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  | Right|
- * `-----------------------------------------------------------------------------------'
+ * ,------------------------------------------------------------------------------------.
+ * | Esc   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |-------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | '"`  |
+ * |-------+------+------+------+------+-------------+------+------+------+------+------|
+ * |   `   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Enter|
+ * |-------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /\  | Shift|
+ * |-------+------+------+------+------+------+------+------+------+------+------+------|
+ * |MicMute| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  | Right|
+ * `------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD_QUOT,
     TD_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD_SCLN, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  TD_SLSH, KC_RSFT,
-    _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    MICMUTE, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -413,6 +414,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
         }
         return false;
+    case MICMUTE:
+        if (record->event.pressed) {
+           SEND_STRING(SS_LGUI("y")); // Cmd-Y for chime
+        }
+        break;
     }
     return true;
 }
