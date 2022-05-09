978e4c08a8b30cd1b644da0132783a2b3ca7ce84
Patrick Tsai <patrick@simplypatrick.com>
2022-05-08 22:09:26 -0700

    layers: rename layer enum

--- keymap.c
+++ keymap.c
@@ -274,10 +274,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 // Layers
 
 enum preonic_layers {
-    _QWERTY,
-    _LOWER,
-    _RAISE,
-    _ADJUST,
+    LAYER_QWERTY,
+    LAYER_LOWER,
+    LAYER_RAISE,
+    LAYER_ADJUST,
 };
 
 enum preonic_keycodes {
@@ -306,7 +306,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |MicMute| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  | Right|
  * `------------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT_preonic_grid(
+[LAYER_QWERTY] = LAYOUT_preonic_grid(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD_QUOT,
     TD_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD_SCLN, KC_ENT,
@@ -327,7 +327,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_preonic_grid(
+[LAYER_LOWER] = LAYOUT_preonic_grid(
     KC_DEL,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL,
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_PIPE,
     KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, _______,
@@ -348,7 +348,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_preonic_grid(
+[LAYER_RAISE] = LAYOUT_preonic_grid(
     KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
@@ -369,7 +369,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite|      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_preonic_grid(
+[LAYER_ADJUST] = LAYOUT_preonic_grid(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, _______,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______,  _______, _______, _______, _______,
@@ -383,20 +383,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case LOWER:
         if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            layer_on(LAYER_LOWER);
+            update_tri_layer(LAYER_LOWER, LAYER_RAISE, LAYER_ADJUST);
         } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            layer_off(LAYER_LOWER);
+            update_tri_layer(LAYER_LOWER, LAYER_RAISE, LAYER_ADJUST);
         }
         return false;
     case RAISE:
         if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            layer_on(LAYER_RAISE);
+            update_tri_layer(LAYER_LOWER, LAYER_RAISE, LAYER_ADJUST);
         } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            layer_off(LAYER_RAISE);
+            update_tri_layer(LAYER_LOWER, LAYER_RAISE, LAYER_ADJUST);
         }
         return false;
     case BACKLIT:
@@ -437,7 +437,7 @@ uint16_t muse_tempo = 50;
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (muse_mode) {
-        if (IS_LAYER_ON(_RAISE)) {
+        if (IS_LAYER_ON(LAYER_RAISE)) {
             if (clockwise) {
                 muse_offset++;
             } else {
@@ -466,9 +466,9 @@ bool dip_switch_update_user(uint8_t index, bool active) {
     switch (index) {
     case 0:
         if (active) {
-            layer_on(_ADJUST);
+            layer_on(LAYER_ADJUST);
         } else {
-            layer_off(_ADJUST);
+            layer_off(LAYER_ADJUST);
         }
         break;
     case 1:
