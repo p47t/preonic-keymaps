6b005cf886e1303bdd04a6fab724aa00e2f7c330
Patrick Tsai <patrick@simplypatrick.com>
2022-05-14 23:16:00 -0700

    lower: add mouse simulatation

--- keymap.c
+++ keymap.c
@@ -318,23 +318,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------------------.
- * |  Del  |   !   |   @   |   #   |   $   |   %   |   ^   |   &   |   *   |   (   |   )   |  Del  |
+ * |   Del |   !   |   @   |   #   |   $   |   %   |   ^   |   &   |   *   |   (   |   )   |  Del  |
  * |-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------|
- * |   ~   |   !   |   @   |   #   |   $   |   %   |   ^   |   &   |   *   |   [   |   ]   |   |   |
+ * |   ~   |   B1  |   MU  |  B2   |       |       |       |       |       |   [   |   ]   |   |   |
  * |-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------|
- * |   ~   |  F1   |  F2   |  F3   |  F4   |  F5   |  F6   |   _   |   +   |   {   |   }   |       |
+ * |   ~   |   ML  |   MD  |  MR   |       |       |       |   _   |   +   |   {   |   }   |       |
  * |-------+-------+-------+-------+-------+-------|-------+-------+-------+-------+-------+-------|
- * |       |  F7   |  F8   |  F9   |  F10  |  F11  |  F12  |ISO ~  |ISO |  | Home  | End   |       |
+ * |   _   |   WL  |   WD  |  WU   |  WR   |       |       | ISO ~ | ISO | |  Home |  End  |   +   |
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
  * |       |       |       |       |       |               |       | Next  | Vol-  | Vol+  |  Play |
  * `-----------------------------------------------------------------------------------------------'
  */
 [LAYER_LOWER] = LAYOUT_preonic_grid(
     KC_DEL,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL,
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LBRC, KC_RBRC, KC_PIPE,
-    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, _______,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_TILD, KC_BTN1, KC_MS_U, KC_BTN2, _______, _______, _______, _______,    _______,    KC_LBRC, KC_RBRC, KC_PIPE,
+    KC_TILD, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, _______, KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, _______,
+    _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  KC_PLUS,
+    _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
