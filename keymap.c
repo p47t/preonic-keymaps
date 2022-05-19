ce09134cfd4e88c8a6998dfd5c838f97a2749077
Patrick Tsai <patrick@simplypatrick.com>
2022-05-18 19:13:47 -0700

    macros: Add placeholders for macros

--- keymap.c
+++ keymap.c
@@ -194,6 +194,11 @@ enum preonic_keycodes {
     BACKLIT,
     TRAILING_SEMICOLON,
     MICMUTE, // mute microphone
+    MACKEY1,
+    MACKEY2,
+    MACKEY3,
+    MACKEY4,
+    MACKEY5,
 };
 
 #define MT_LSFT MT(MOD_LSFT, KC_MINUS)
@@ -254,7 +259,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------|
  * |   `   |       |       |       |       |       | Left  | Down  |  Up   | Right |       |       |
  * |-------+-------+-------+-------+-------+-------|-------+-------+-------+-------+-------+-------|
- * |   _   |       |       |       |       |       |       |       |       |       |       |   +   |
+ * |   _   | Macro | Macro | Macro | Macro | Macro |       |       |       |       |       |   +   |
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
  * |       |       |       |       |       |               |       | Next  | Vol-  | Vol+  |  Play |
  * `-----------------------------------------------------------------------------------------------'
@@ -263,7 +268,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
     KC_GRV,  KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_BSLS,
     KC_GRV,  _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    KC_UNDS, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PLUS,
+    KC_UNDS, MACKEY1, MACKEY2, MACKEY3, MACKEY4, MACKEY5, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PLUS,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
