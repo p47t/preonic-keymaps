c55c55588b5c65af89295814d2b0022b8229620e
Patrick Tsai <yingtsai@amazon.com>
2022-05-20 23:11:41 -0700

    base: Remove unused code

--- keymap.c
+++ keymap.c
@@ -304,18 +304,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case LGUI_T(KC_LCBR):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_LCBR); // Send KC_LCBR on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
-    case LCTL_T(KC_LPRN):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_LPRN); // Send KC_DQUO on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
     case LOWER:
         if (record->event.pressed) {
             layer_on(LAYER_LOWER);
